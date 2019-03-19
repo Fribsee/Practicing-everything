66a43cb08ba6d018beb57ec00b5a2caf758461b1
fribsee <40807715+Fribsee@users.noreply.github.com>
2019-03-18 19:00:11 -0500

    TitleCase pipe is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StarComponent } from './star/star.component';
-import { ServerComponent } from './server/server.component';
+import { TitleCaseComponent } from './title-case/title-case.component';
+import { TitleCasePipe } from './titlecase.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     StarComponent,
-    ServerComponent
+    TitleCaseComponent,
+    TitleCasePipe,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
