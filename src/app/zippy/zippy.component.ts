763f40adb187477bff67e489614328414bc39d5d
fribsee <40807715+Fribsee@users.noreply.github.com>
2019-03-25 18:27:04 -0500

    created dropdown 'zippy' for shipping details

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -8,5 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ZippyComponent  {
 @Input('title')title: string;
 
+isExpanded: boolean;
 
+ toggle() {
+   this.isExpanded = !this.isExpanded;
+ }
 }
